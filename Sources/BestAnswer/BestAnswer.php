84a48066b2d6c29616ef4736fab7bd42aa4a2f3c
SychO9 <sychocouldy@gmail.com>
2020-08-11 22:52:05 +0100

    Add mod credits

--- Sources/BestAnswer/BestAnswer.php
+++ Sources/BestAnswer/BestAnswer.php
@@ -11,6 +11,11 @@ namespace SychO\BestAnswer;
 class BestAnswer
 {
 	/**
+	 * @var string
+	 */
+	const VERSION = '0.1.0';
+
+	/**
 	 * Registers the rest of the hooks
 	 * --------------------------
 	 * Easier this way than having to register the hooks in the database,
@@ -22,5 +27,12 @@ class BestAnswer
 		Permissions::init();
 		TopicView::init();
 		TopicListView::init();
+
+		add_integration_function('integrate_credits', '\SychO\BestAnswer\BestAnswer::addCredits', false);
+	}
+
+	public static function addCredits()
+	{
+		$GLOBALS['context']['copyrights']['mods'][] = '<a href="https://github.com/SychO9/smf-best-answer">Best Answer v'.self::VERSION.'</a> by <a href="https://github.com/SychO9">SychO</a> &copy; 2020 | Licensed under <a href="http://en.wikipedia.org/wiki/MIT_License" target="_blank" rel="noopener">The MIT License (MIT)</a>';
 	}
 }
