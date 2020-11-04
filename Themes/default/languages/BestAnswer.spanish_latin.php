ba130b18f19e535820d15e0988b317bf989fd178
Diego Andrés <diegoandres_cortes@outlook.com>
2020-11-04 11:08:53 -0600

    Add spanish translation
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Themes/default/languages/BestAnswer.spanish_latin.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package BestAnswer
+ * @author Sami "SychO" Mazouz (sychocouldy@gmail.com)
+ * @license MIT
+ */
+
+$txt['best_answer'] = 'Mejor Respuesta';
+$txt['mark_as_best'] = 'Marcar como mejor respuesta';
+$txt['unmark_as_best'] = 'Desmarcar como mejor respuesta';
+
+$txt['permissionname_mark_best_answer'] = 'Marcar un post como mejor respuesta en un tema';
+$txt['permissionname_mark_best_answer_any'] = 'Temas Propios';
+$txt['permissionname_mark_best_answer_own'] = 'Cualquier Tema';
+$txt['permissionhelp_mark_best_answer'] = 'Con este permiso el usuario podrá marcar un post como la mejor respuesta.';
+
+$txt['setting_best_answer_boards'] = 'Activar Mejor Respuesta en los siguientes foros';
